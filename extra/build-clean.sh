de942cba481e2e97e1c301f8bce1314d72c5eaf4
Yamashita TAKAO <tjy1965@gmail.com>
2011-10-27 09:41:42 +0900

    rename

--- extra/build-clean.sh
+++ extra/build-clean.sh
@@ -3,13 +3,13 @@
 wdir=$(readlink -f $(dirname $0))
 
 echo 'rm /system/lib/modules/*' > ${wdir}/setup/clean.sh
-for f in $(cat system.apps)
+for f in $(cat base.list)
 do
 	name=$(basename $f .apk)
 	echo 'rm /system/app/'$name'*.apk'
 done >> ${wdir}/setup/clean.sh
 
-for f in $(cat extra.apps)
+for f in $(cat extra.list)
 do
 	name=$(basename $f .apk)
 	echo 'rm /data/app/'$name'*.apk'
