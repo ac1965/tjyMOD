6e898a0d0a2b35b607dadce9c92caa156ac3934c
Yamashita TAKAO <tjy1965@gmail.com>
2011-11-13 09:00:31 +0900

    fixed typo

--- script/meta.sh
+++ script/meta.sh
@@ -5,7 +5,7 @@ export LANG=C
 
 workdir="$(readlink -f $(dirname $0))"
 dt=$(date +%Y%m%d)
-myname=$(dirname $0)
+myname=$(basename $0)
 
 . $workdir/setting.sh || exit 1
 . $workdir/core.sh || exit 1
