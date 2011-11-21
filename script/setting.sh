0a00665befe9116a32c899e78c8876d2a284d776
ac1965 <tjy1965@gmail.com>
2011-11-21 11:49:28 +0900

    update version and baserom, kernel image

--- script/setting.sh
+++ script/setting.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 
 PKG=tjyMOD
-VERSION=0.76
+VERSION=0.77
 PKGNAME=${PKG}_v${VERSION}
 
 # I hope to put site my KANG, kernel and ROM.
 giturl="git://github.com/ac1965/tjyMOD.git"
 default_url="http://tjy.sakura.ne.jp/pu/up/android"
 # LorDmodUE-8.3-CFS-b1-aufs
-default_kernel="update_2.6.35-BFS-WIP-AUFS_201111071812.zip"
-default_baserom="update-cm-7.1.0-DesireHD-KANG_201111120843.signed.zip"
+default_kernel="update_2.6.35-BFS-WIP-AUFS_201111211050.zip"
+default_baserom="update-cm-7.1.0-DesireHD-KANG_201111201027.signed.zip"
 default_gapps="gapps-gb-20110930-237-signed.zip"
 
 base_list="
